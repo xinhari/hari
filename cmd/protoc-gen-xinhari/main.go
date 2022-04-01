d25124f1af65d6e2c640a30680955f5556dd6abf
Agus Imam Fauzi <agus7fauzi@gmail.com>
2022-04-01 13:27:07 +0700

    fix xinhari proto plugin dependency

--- cmd/protoc-gen-xinhari/main.go
+++ cmd/protoc-gen-xinhari/main.go
@@ -52,8 +52,8 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/ebelanja/micro/cmd/protoc-gen-micro/generator"
-	_ "github.com/ebelanja/micro/cmd/protoc-gen-micro/plugin/xinhari"
+	"github.com/ebelanja/micro/cmd/protoc-gen-xinhari/generator"
+	_ "github.com/ebelanja/micro/cmd/protoc-gen-xinhari/plugin/xinhari"
 	"github.com/golang/protobuf/proto"
 )
 
