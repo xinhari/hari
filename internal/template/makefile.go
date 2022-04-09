2a8f1230f23fed42425fdb8f32cb66a8b8e0e34a
Agus Imam Fauzi <agus7fauzi@gmail.com>
2022-04-09 23:46:40 +0700

    fix makefile template

--- internal/template/makefile.go
+++ internal/template/makefile.go
@@ -4,13 +4,20 @@ var (
 	Makefile = `
 GOPATH:=$(shell go env GOPATH)
 MODIFY=Mproto/imports/api.proto=xinhari.com/xinhari/api/proto
+.PHONY: init
+init:
+	go install github.com/golang/protobuf/proto
+	go install github.com/golang/protobuf/protoc-gen-go
+	go install github.com/xinhari/hari/cmd/protoc-gen-xinhari
+	go install github.com/cosmtrek/air@latest
+
 {{if ne .Type "web"}}
 .PHONY: proto
 proto:
     {{if eq .UseGoPath true}}
-	protoc --proto_path=${GOPATH}/src:. --micro_out=${MODIFY}:. --go_out=${MODIFY}. proto/{{.Alias}}/{{.Alias}}.proto
+	protoc --proto_path=${GOPATH}/src:. --xinhari_out=${MODIFY}:. --go_out=${MODIFY}. proto/{{.Alias}}/{{.Alias}}.proto
     {{else}}
-	protoc --proto_path=. --micro_out=${MODIFY}:. --go_out=${MODIFY}:. proto/{{.Alias}}/{{.Alias}}.proto
+	protoc --proto_path=. --xinhari_out=${MODIFY}:. --go_out=${MODIFY}:. proto/{{.Alias}}/{{.Alias}}.proto
     {{end}}
 
 .PHONY: build
@@ -20,7 +27,9 @@ build: proto
 build:
 {{end}}
 	go build -o {{.Alias}}-{{.Type}} *.go
-
+.PHONY: dev
+dev:
+	air
 .PHONY: test
 test:
 	go test -v ./... -cover
